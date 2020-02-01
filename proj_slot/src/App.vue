f5aac796337bb2967a68935a9454de5b5fb005ac
cdacpune1 <50952547+cdacpune1@users.noreply.github.com>
2020-02-01 16:56:23 +0530

    Add files via upload

---
+++ proj_slot/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <form-helper>
+     <div slot="form-header">
+       <h3>This is title of form</h3>
+       <p>Info about the form</p>
+     </div>
+     <div slot="form-fields">
+       <input type="text" placeholder="name" required/>
+         <input type="password" placeholder="pass" required/>
+     </div>
+     <div slot="form-control">
+       <button v-on:click="submit">Submit</button>
+     </div>
+    </form-helper>
+  </div>
+</template>
+
+<script>
+import fromHelper from'./components/formHelper.vue'
+export default {
+ components:{
+'form-helper':fromHelper
+ },
+  data () {
+    return {
+      title:'I am a dynamic slot title'
+      
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style>
+
+</style>
