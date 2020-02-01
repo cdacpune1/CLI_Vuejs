75ea6a892571e16f768d59a02c742dbf301a1a4a
cdacpune1 <50952547+cdacpune1@users.noreply.github.com>
2020-02-01 16:55:22 +0530

    Add files via upload

---
+++ myFirst-Proj/src/component/Footer.vue
@@ -0,0 +1,39 @@
+ <template>
+ <footer>
+     <p>{{copyright}}.{{title}}</p>
+ </footer>
+</template>
+
+<script>
+import {bus} from'../main'
+export default {
+props:{
+    title:{
+ type:String
+    }
+},
+
+  data() {
+    return {
+        copyright:'copyright 2017 Ninjas'
+    }
+  },
+  created(){
+      bus.$on('titlechng',(data) => {
+          this.title = data;
+      })
+  }
+}
+</script>
+
+<style scoped>
+footer{
+    background:gray;
+    padding: 6px;
+
+}
+p{
+    color:ivory;
+    text-align: center;
+}
+</style>
---
+++ myFirst-Proj/src/component/Header.vue
@@ -0,0 +1,39 @@
+<template>
+<header>
+    <h1 v-on:click="chng">{{title}}</h1>
+</header>
+</template>
+
+<script>
+import {bus} from'../main'   
+export default {
+    props:{
+        title:{
+            type:String
+        }
+    },
+  data() {
+    return {
+     title:'Vue Ninja'
+    }
+  },
+  methods:{
+      chng:function(){
+// this.$emit('chng','VueWiards');
+this.title = 'Vue Wizards';
+bus.$emit('titlechng','Vue Wizards');
+      }
+  }
+}
+</script>
+<style scoped>
+header{
+    background:green;
+    padding: 10px;
+
+}
+h1{
+color:whitesmoke;
+text-align: center;
+}
+</style>
